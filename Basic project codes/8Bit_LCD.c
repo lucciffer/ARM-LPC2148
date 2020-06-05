5bb443e444f54ab231cea09c0eb83556f963ca6f
Lucciffer <51166059+lucciffer@users.noreply.github.com>
2020-06-05 18:12:31 +0530

    Update 8Bit_LCD.c

--- Basic project codes/8Bit_LCD.c
+++ Basic project codes/8Bit_LCD.c
@@ -29,7 +29,7 @@ int main()
     LCD_SetUp(P1_27,P1_28,P1_29,P1_16,P1_17,P1_18,P1_19,P1_20,P1_21,P1_22,P1_23);
     LCD_Init(2,16);
     
-    LCD_DisplayString("Explore Embedded");
+    LCD_DisplayString("TechhEngine");
     LCD_DisplayString("Lcd 8-bit Mode");
 
     while(1);
